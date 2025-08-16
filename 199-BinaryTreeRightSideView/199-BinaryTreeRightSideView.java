40b2c3d1238df6b0b321be1ea90da375cec630c2
ayushgoswami666 <151738163+ayushgoswami666@users.noreply.github.com>
2025-08-16 23:14:37 +0530

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.java
@@ -0,0 +1,38 @@
+// Last updated: 8/16/2025, 11:14:36 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max_depth=0;
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> l1 = new ArrayList<>();
+        rightview(root,1,l1);
+        return l1;
+        
+    }
+    public void rightview(TreeNode root,int curr_level,List<Integer> l1){
+        if(root == null){
+            return ;
+        }
+        if(max_depth<curr_level){
+            l1.add(root.val);
+            max_depth=curr_level;
+        }
+
+        rightview(root.right,curr_level+1,l1);
+        rightview(root.left,curr_level+1,l1);
+
+    }
+}
