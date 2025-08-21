ee346e6c9265a6cbb45f6d8a4ff161519198e8ca
ayushgoswami666 <151738163+ayushgoswami666@users.noreply.github.com>
2025-08-21 12:07:56 +0530

    File updated at 8/21/2025, 12:07:54 PM

--- 502-IPO/502-IPO.java
+++ 502-IPO/502-IPO.java
@@ -1,4 +1,4 @@
-// Last updated: 8/21/2025, 12:06:58 PM
+// Last updated: 8/21/2025, 12:07:54 PM
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         
