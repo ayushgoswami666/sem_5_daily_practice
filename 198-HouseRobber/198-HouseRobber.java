b7288fd7b4f779a8f3f4a48c4663bcfd65cb2013
ayushgoswami666 <151738163+ayushgoswami666@users.noreply.github.com>
2025-09-03 10:56:38 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,17 @@
+// Last updated: 9/3/2025, 10:56:36 AM
+class Solution {
+    public int rob(int[] nums) {
+        int rob1 =0;
+        int rob2 = 0;
+        int max = 0;
+
+        for( int i =0;i<nums.length;i++){
+            max = Math.max(rob1 +nums[i],rob2);
+            rob1 =rob2;
+            rob2 = max;
+        }
+        return max;
+        
+    }
+    
+}
