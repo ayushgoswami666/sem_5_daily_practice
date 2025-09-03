c28815773f8daf23f7d6377fcad00f6e1c6e8c2d
ayushgoswami666 <151738163+ayushgoswami666@users.noreply.github.com>
2025-09-03 11:59:43 +0530

    File updated at 9/3/2025, 11:59:40 AM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,17 +1,20 @@
-// Last updated: 9/3/2025, 10:56:36 AM
+// Last updated: 9/3/2025, 11:59:40 AM
 class Solution {
     public int rob(int[] nums) {
-        int rob1 =0;
-        int rob2 = 0;
-        int max = 0;
-
-        for( int i =0;i<nums.length;i++){
-            max = Math.max(rob1 +nums[i],rob2);
-            rob1 =rob2;
-            rob2 = max;
-        }
-        return max;
-        
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return Robber(nums, 0, dp);
     }
-    
+
+    public static int Robber(int[] nums, int i, int[] dp) {
+        if (i >= nums.length) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        int rob = nums[i] + Robber(nums, i + 2, dp);
+        int dontRob = Robber(nums, i + 1, dp);
+        return dp[i] = Math.max(rob, dontRob);
+    }
 }
