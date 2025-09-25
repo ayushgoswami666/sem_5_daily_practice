7f705af9b8c49a3961eb069928d9d8e77309eb84
ayushgoswami666 <151738163+ayushgoswami666@users.noreply.github.com>
2025-09-25 15:11:03 +0530

    File updated at 9/25/2025, 3:11:01 PM

--- 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.java
+++ 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.java
@@ -1,12 +1,13 @@
-// Last updated: 9/25/2025, 3:09:12 PM
+// Last updated: 9/25/2025, 3:11:01 PM
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
+         Arrays.sort(nums);
         ArrayList<Integer> l1 = new ArrayList<>();
-        Arrays.sort(nums);
+       
         for(int i =0;i<nums.length;i++){
-            if(nums[i]==target){
-                l1.add(i);
-            }
+            if(nums[i]==target) l1.add(i);
+                
+            
         }
         return l1;
         
