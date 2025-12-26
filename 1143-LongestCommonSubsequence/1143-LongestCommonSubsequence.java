c91fb2b9d96598808980ea447a9b9813eff1f78f
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2025-12-26 10:15:55 +0530

    File updated at 12/26/2025, 10:15:43 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,31 +1,30 @@
-// Last updated: 9/4/2025, 11:55:25 AM
-class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int [text1.length()][text2.length()];
-        for(int[] a: dp){
-            Arrays.fill(a,-1);
-        }
-        return Lcs(text1,text2,0,0,dp);
-       
-        
-    }
-    public static int Lcs(String s1,String s2,int i,int j,int[][] dp){
-
-        if(i == s1.length() || j == s2.length()){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int ans =0;
-        if(s1.charAt(i) == s2.charAt(j)){
-            ans = 1 + Lcs(s1,s2,i+1,j+1,dp);
-        }
-        else{
-            int f = Lcs(s1,s2,i+1,j,dp);
-            int s = Lcs(s1,s2,i,j+1,dp);
-            ans = Math.max(f,s);
-        }
-        return dp[i][j]=ans;
-    }
-}
+// Last updated: 12/26/2025, 10:15:43 AM
+1class Solution {
+2    public int longestCommonSubsequence(String text1, String text2) {
+3        int[][] dp = new int[text1.length()][text2.length()];
+4        for(int[] row : dp){
+5            Arrays.fill(row,-1);
+6        }
+7        return lcs(text1,text2,0,0,dp);
+8
+9        
+10    }
+11    public static int lcs(String s1,String s2,int i,int j,int[][] dp){
+12        if(s1.length()==i || s2.length()==j){
+13            return 0;
+14        }
+15        if(dp[i][j]!=-1){
+16            return dp[i][j];
+17        }
+18        int ans = 0;
+19        if(s1.charAt(i)==s2.charAt(j)){
+20            ans = 1+lcs(s1,s2,i+1,j+1,dp);
+21        }
+22        else{
+23            int f = lcs(s1,s2,i+1,j,dp);
+24            int s = lcs(s1,s2,i,j+1,dp);
+25            ans = Math.max(f,s);
+26        }
+27        return dp[i][j] = ans;
+28    }
+29}
