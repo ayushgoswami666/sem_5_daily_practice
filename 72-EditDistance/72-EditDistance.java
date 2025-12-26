a49d18018b04143097e3ff44089f1e774119c9bb
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2025-12-26 23:26:23 +0530

    File updated at 12/26/2025, 11:26:22 PM

--- 72-EditDistance/72-EditDistance.java
+++ 72-EditDistance/72-EditDistance.java
@@ -1,25 +1,30 @@
-// Last updated: 10/21/2025, 10:00:31 PM
-class Solution {
-  public int minDistance(String word1, String word2) {
-    final int m = word1.length();//first word length
-    final int n = word2.length();///second word length
-    // dp[i][j] := min # of operations to convert word1[0..i) to word2[0..j)
-    int[][] dp = new int[m + 1][n + 1];
-
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1.charAt(i - 1) == word2.charAt(j - 1))//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;                 
-                                //replace               //delete        //insert
-
-    return dp[m][n];
-  }
-}
+// Last updated: 12/26/2025, 11:26:22 PM
+1class Solution {
+2    public int minDistance(String word1, String word2) {
+3        int[][] dp = new int[word1.length()][word2.length()];
+4        return editdistance(word1,word2,0,0,dp);
+5        
+6    }
+7    public  static int editdistance(String s,String t,int i ,int j,int[][] dp ){
+8        if(i==s.length()){
+9            return t.length()-j;
+10        }
+11        if(j==t.length()){
+12            return s.length()-i;
+13        }
+14        if(dp[i][j]!=0){
+15            return dp[i][j];
+16        }
+17        int ans =0;
+18        if(s.charAt(i)==t.charAt(j)){
+19            ans = editdistance(s,t,i+1,j+1,dp);
+20        }
+21        else{
+22            int d = editdistance(s,t,i+1,j,dp);
+23            int I = editdistance(s,t,i,j+1,dp);
+24            int R = editdistance(s,t,i+1,j+1,dp);
+25            ans = 1+Math.min(R,Math.min(d,I)); 
+26        }
+27        return dp[i][j]= ans;
+28    }
+29}
