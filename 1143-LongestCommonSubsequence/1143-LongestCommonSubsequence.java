e085b3d12e84068a8afb88e0e3da071ee9b58461
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2025-12-26 10:41:35 +0530

    File updated at 12/26/2025, 10:41:22 AM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,30 +1,24 @@
-// Last updated: 12/26/2025, 10:15:43 AM
+// Last updated: 12/26/2025, 10:41:22 AM
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
-3        int[][] dp = new int[text1.length()][text2.length()];
-4        for(int[] row : dp){
-5            Arrays.fill(row,-1);
-6        }
-7        return lcs(text1,text2,0,0,dp);
-8
-9        
-10    }
-11    public static int lcs(String s1,String s2,int i,int j,int[][] dp){
-12        if(s1.length()==i || s2.length()==j){
-13            return 0;
-14        }
-15        if(dp[i][j]!=-1){
-16            return dp[i][j];
-17        }
-18        int ans = 0;
-19        if(s1.charAt(i)==s2.charAt(j)){
-20            ans = 1+lcs(s1,s2,i+1,j+1,dp);
-21        }
-22        else{
-23            int f = lcs(s1,s2,i+1,j,dp);
-24            int s = lcs(s1,s2,i,j+1,dp);
-25            ans = Math.max(f,s);
-26        }
-27        return dp[i][j] = ans;
-28    }
-29}
+3        return lcsBU(text1,text2);
+4        
+5    }
+6    public static int lcsBU(String s1,String s2){
+7        int[][] dp = new int[s1.length()+1][s2.length()+1];
+8        // to handel the blank string
+9        for(int i =1;i<dp.length;i++){
+10            for(int j = 1;j<dp[0].length;j++){
+11                if(s1.charAt(i-1)==s2.charAt(j-1)){
+12                    dp[i][j] = 1+dp[i-1][j-1];
+13                }
+14                else{
+15                    int f = dp[i][j-1];
+16                    int s = dp[i-1][j];
+17                    dp[i][j] = Math.max(f,s);
+18                }
+19            }
+20        }
+21        return dp[dp.length-1][dp[0].length-1];
+22    }
+23}
