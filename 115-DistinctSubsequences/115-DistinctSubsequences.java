4893fa05c3d439aa06b759c92627e948f00123c4
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2025-12-26 17:41:37 +0530

    File updated at 12/26/2025, 5:41:36 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.java
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -1,27 +1,28 @@
-// Last updated: 12/9/2025, 11:15:08 PM
+// Last updated: 12/26/2025, 5:41:36 PM
 1class Solution {
-2    public int numDistinct(String B, String A) {
-3         int lenA= A.length();
-4        int lenB = B.length();
-5        Integer [][] memo = new Integer [lenA][lenB];
-6
-7        return solve(A,B,lenA-1,lenB-1,memo);
-8
+2    public int numDistinct(String s, String t) {
+3        int[][] dp = new int[s.length()][t.length()];
+4        for(int[] row : dp){
+5            Arrays.fill(row,-1);
+6        }
+7        return subseq(s,t,0,0,dp);
+8        
 9    }
-10
-11    private int solve(String A, String B, int i, int j, Integer[][]memo){
-12
-13        if(i<0) return 1;
-14
-15        if(j<0) return 0;
-16
-17        if(j<i) return 0;
-18
-19        if(memo [i][j] != null) return memo [i][j];
-20
-21        if(A.charAt(i) == B.charAt(j)){
-22            return memo [i][j] = solve(A,B,i-1,j-1,memo) + solve(A,B,i,j-1,memo);
+10    public static int  subseq(String s,String t,int i,int j,int[][] dp){
+11        if(j==t.length()){
+12            return 1;
+13        }
+14        if(i==s.length()){
+15            return 0;
+16        }
+17        if(dp[i][j]!=-1){
+18            return dp[i][j];
+19        }
+20        int inc =0,exc=0;
+21        if(s.charAt(i)==t.charAt(j)){
+22             inc = subseq(s,t,i+1,j+1,dp);
 23        }
-24        return memo[i][j] = solve(A,B,i,j-1,memo);
-25    }
-26}
+24          exc = subseq(s,t,i+1,j,dp);
+25        return dp[i][j] = inc + exc;
+26    }
+27}
