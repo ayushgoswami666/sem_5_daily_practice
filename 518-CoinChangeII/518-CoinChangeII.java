b903c2cea37a3b9911e290e62a7a1f8065e1b602
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2025-12-26 12:24:53 +0530

    File updated at 12/26/2025, 12:24:52 PM

--- 518-CoinChangeII/518-CoinChangeII.java
+++ 518-CoinChangeII/518-CoinChangeII.java
@@ -1,31 +1,25 @@
-// Last updated: 9/4/2025, 11:36:40 AM
-class Solution {
-    public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount +1][coins.length];
-        for(int[] a :dp){
-            Arrays.fill(a,-1);
-        }
-        return CC(amount,coins,0,dp);
-        
-    }
-    public static int CC(int amount,int[] coins,int i,int[][] dp){
-
-        if(amount == 0){
-            return 1;
-        }
-        if(i == coins.length){
-            return 0;
-        }
-        if(dp[amount][i] !=-1){
-            return dp[amount][i];
-
-        }
-        int inc = 0,exc =0;
-        if(amount>=coins[i]){
-            inc = CC(amount-coins[i],coins,i,dp);
-
-        }
-        exc = CC(amount,coins,i+1,dp);
-        return dp[amount][i] = inc +exc;
-    }
-}
+// Last updated: 12/26/2025, 12:24:52 PM
+1class Solution {
+2    public int change(int amount, int[] coins) {
+3        return CoinChange(amount,coins);
+4        
+5    }
+6    public static int CoinChange(int amount, int[] coin){
+7        int[][] dp = new int[amount+1][coin.length+1];
+8        for(int i =0;i<dp[0].length;i++){
+9            dp[0][i]= 1;
+10        }
+11        for(int i = 1;i<dp.length;i++){
+12            for(int j = 1;j<dp[0].length;j++){
+13                int inc =0;
+14                int exc = 0;
+15                if(coin[j-1]<=i){
+16                    inc = dp[i-coin[j-1]][j];
+17                }
+18                exc = dp[i][j-1];
+19                dp[i][j] = inc+exc;
+20            }
+21        }
+22        return dp[dp.length-1][dp[0].length-1];
+23    }
+24}
