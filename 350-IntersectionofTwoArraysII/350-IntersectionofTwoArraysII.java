c764183abd7121356cd7ab446942594ab5fe6451
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2026-01-13 15:08:37 +0530

    File updated at 1/13/2026, 3:08:35 PM

--- 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.java
+++ 350-IntersectionofTwoArraysII/350-IntersectionofTwoArraysII.java
@@ -1,39 +1,51 @@
-// Last updated: 1/13/2026, 1:24:16 PM
+// Last updated: 1/13/2026, 3:08:35 PM
 1class Solution {
-2    public int[] intersection(int[] nums1, int[] nums2) {
-3        return Two_arr(nums1,nums2);
-4    }
-5         
-6    public static int[] Two_arr(int[] nums1,int[] nums2){
-7        HashMap<Integer,Integer> map = new HashMap<>();
-8        for(int i =0;i<nums1.length;i++){
-9            if(map.containsKey(nums1[i])){
-10                // int v = map.get(nums1[i]);
-11                // map.put(nums1[i],v+1);
-12
-13            }
-14            else{
-15                map.put(nums1[i],1);
-16            }
-17        }
-18        List<Integer> ll = new ArrayList<>();
-19        for(int i =0;i<nums2.length;i++){
-20            if(map.containsKey(nums2[i]) && map.get(nums2[i])>0){
-21                ll.add(nums2[i]);
-22                map.put(nums2[i],map.get(nums2[i])-1);
-23            }
-24
-25        }
-26        int[] ans = new int[ll.size()];
-27        for(int i =0;i<ans.length;i++){
-28            ans[i] = ll.get(i);
-29        }
-30        return ans;
-31    }
-32                
-33                
-34                
-35
-36        
-37    
-38}
+2    public int longestConsecutive(int[] nums) {
+3
+4        // HashSet<Integer> set = new HashSet<>();
+5        // int ans =0;
+6        // for(int i=0;i<nums.length;i++){
+7        //     set.add(nums[i]);
+8
+9        // }
+10        // for(int i=0;i<nums.length;i++){
+11        //     if(!set.contains(nums[i]-1)){
+12        //         int c =0;
+13        //         int key=nums[i];
+14
+15        //         while(set.contains(key)){
+16        //             set.remove(key);
+17        //             c++;
+18        //             key++;
+19        //         }
+20        //         ans= Math.max(ans,c);
+21        //     }
+22        // }
+23        // return ans;
+24        HashMap<Integer,Boolean> map = new HashMap<>();
+25        for(int i =0;i<nums.length;i++){
+26            if(map.containsKey(nums[i]-1)){
+27                map.put(nums[i],false);
+28            }
+29            else{
+30                map.put(nums[i],true);
+31            }
+32            if(map.containsKey(nums[i]+1)){
+33                map.put(nums[i]+1,false);
+34            }
+35        }
+36        int ans = 0;
+37        for(int key : map.keySet()){
+38            if(map.get(key)){
+39                int count= 0;
+40                while(map.containsKey(key)){
+41                    count++;
+42                    key++;
+43                }
+44                ans = Math.max(ans,count);
+45            }
+46        }
+47        return ans;
+48        
+49    }
+50}
