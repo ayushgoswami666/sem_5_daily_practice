3149bb3f428e7984f34a291896519c8d93338df9
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2026-01-21 12:37:35 +0530

    File updated at 1/21/2026, 12:37:33 PM

--- 1835-FindXORSumofAllPairsBitwiseAND/1835-FindXORSumofAllPairsBitwiseAND.java
+++ 1835-FindXORSumofAllPairsBitwiseAND/1835-FindXORSumofAllPairsBitwiseAND.java
@@ -1,17 +1,38 @@
-// Last updated: 1/21/2026, 12:13:37 PM
+// Last updated: 1/21/2026, 12:37:33 PM
 1class Solution {
-2    public int getXORSum(int[] arr1, int[] arr2) {
-3        int xor1 = Bitwise_xor(arr1);
-4        int xor2 = Bitwise_xor(arr2);
-5        int result = xor1 & xor2;
-6       return result;
-7        
-8    }
-9    public static int Bitwise_xor(int[] arr) {
-10        int xor = 0;
-11        for (int num : arr) {
-12            xor ^= num;
-13        }
-14        return xor;
-15    }
-16}
+2    public int minimizeXor(int num1, int num2) {
+3        int set_bit=0;
+4        while(num2>0){
+5            num2 = (num2&(num2-1));
+6            set_bit++;
+7        }
+8        int x =0;
+9        for(int i=30;i>=0;i--){
+10            int mask = (1<<i);
+11            if((num1&mask)!=0){
+12                x = x | mask;//x |=mask;
+13                set_bit--;
+14                if(set_bit==0){
+15                    return x;
+16
+17                }
+18            }
+19        }
+20        for(int i =0;i<=30;i++){
+21            int mask = (1<<i);
+22             if((num1&mask)==0){
+23                x = x | mask;//x |=mask;
+24                set_bit--;
+25                if(set_bit==0){
+26                    return x;
+27
+28                }
+29
+30        }
+31         
+32        
+33    }
+34    return x;
+35    }
+36
+37}
