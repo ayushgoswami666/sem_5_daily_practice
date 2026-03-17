6b00c56c0966484779e0ccbda61543d2c767af5e
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2026-03-17 22:31:43 +0530

    Create 3857-MinimumCosttoSplitintoOnes

---
+++ 3857-MinimumCosttoSplitintoOnes/3857-MinimumCosttoSplitintoOnes.java
@@ -0,0 +1,8 @@
+// Last updated: 3/17/2026, 10:31:41 PM
+1class Solution {
+2    public int minCost(int n) 
+3    {
+4        return (n*(n+1))/2-n;
+5    }
+6//please upvote if you find helpful...
+7}
