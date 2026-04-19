d4ed6374baaa1a6c49224490b193dd1b7503351d
Ayush Goswami <151738163+ayushgoswami666@users.noreply.github.com>
2026-04-19 21:30:12 +0530

    File updated at 4/19/2026, 9:29:56 PM

--- 100-SameTree/100-SameTree.java
+++ 100-SameTree/100-SameTree.java
@@ -1,29 +1,24 @@
-// Last updated: 12/3/2025, 10:09:11 PM
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public boolean isSameTree(TreeNode p, TreeNode q) {
-18        if (p == null && q == null) {
-19            return true;
-20        }
-21        
-22        if (p != null && q != null && p.val == q.val) {
-23            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-24        }
-25        
-26        return false;        
-27    }
-28}
+// Last updated: 4/19/2026, 9:29:56 PM
+1class Solution {
+2    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+3        List<List<Integer>>ans = new ArrayList<>();
+4        List<Integer>list = new ArrayList<>();
+5        list.add(0);
+6        dfs(0,graph,ans,list);
+7
+8        return ans;
+9
+10    }
+11
+12    public void dfs(int vtx,int graph[][], List<List<Integer>>ans, List<Integer>list){
+13        if(vtx==graph.length-1){
+14            ans.add(new ArrayList<>(list));
+15        }
+16
+17        for(int ngbr : graph[vtx]){
+18            list.add(ngbr);
+19            dfs(ngbr,graph,ans,list);
+20            list.remove(list.size()-1);
+21        }
+22    }
+23}
